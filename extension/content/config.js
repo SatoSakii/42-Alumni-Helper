2b88f68d84809d8ddc5e3fb439d301f96a538d4d
SatoSakii <satohaki.ber@gmail.com>
2026-09-20 15:55:54 +0200

    feat(extension): add config to extension, to fetch projects and their experiences

---
+++ extension/content/config.js
@@ -0,0 +1,7 @@
+const CURSUS_ID = 21;
+const LEVELS_URL = "https://raw.githubusercontent.com/fzphr/42insight/main/lib/forty-two/data/experience_21.json";
+const PROJECTS_URL = "https://raw.githubusercontent.com/fzphr/42insight/main/lib/forty-two/data/projects_21.json";
+const CACHE_TTL_STATIC_MS = 7 * 24 * 60 * 60 * 1000;
+const CACHE_TTL_CURSUS_USER_MS = 60 * 60 * 1000;
+const CACHE_TTL_PROJECTS_USERS_MS = 15 * 60 * 1000;
+const WIDGET_LOG_PREFIX = "[42-Alumni-Helper]";
